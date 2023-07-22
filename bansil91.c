a1ad0e5f32fff7671093e4609246d52d80d118c9
bansilghori1 <136827896+bansilghori1@users.noreply.github.com>
2023-07-22 21:49:19 +0630

    first commit

--- bansil91.c
+++ bansil91.c
@@ -1,11 +1,35 @@
-#include<stdio.h>
+/*Q.1 Write a Program to check whether a string is a palindrome or not without using string functions.
+For example,
+Input:Enter the string in order to chack whether it is palindrome or not  
 
+
+Output:
+Given string is a Palindrome.
+*/
+
+#include<stdio.h>
+#include<string.h>
 
 void main (){
 
+char a[50],b[50];
+printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+printf("+  Enter the string in order to chack whether it is palindrome or not  +");
+printf("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n");
 
+printf("The string is :- ");
+gets(a);
 
+strcpy(b,a);
+strrev(b);
 
-
-
+if(strcmp(a,b)==0){
+    printf("---------------------------------\n");
+    printf("  Given string is a Palindrome.\n");
+    printf("---------------------------------");
+}else{
+    printf("-------------------------------------\n");
+    printf("  Given string is not a Palindrome.\n");
+    printf("-------------------------------------\n");
+}
 }
